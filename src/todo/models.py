a1a04840bbafd239ec84f4659d43210533590359
Lakii997 <laki.kalajdzic@gmail.com>
2021-09-02 11:34:25 +0200

    todo

--- src/todo/models.py
+++ src/todo/models.py
@@ -3,17 +3,14 @@ from django.db.models.enums import IntegerChoices
 from src.users.models import User
 from .constants import PRIORITY_CHOICES
 
-
-    
-
-
 class ToDo(models.Model):
     title = models.CharField(max_length = 255)
     user = models.ForeignKey(User, on_delete = models.CASCADE)
 
     class ToDoPriority(IntegerChoices):
          LOW = PRIORITY_CHOICES.get('LOW')
-         MEDIUM = PRIORITY_CHOICES.GET('MEDIUM')
+         MEDIUM = PRIORITY_CHOICES.get('MEDIUM')
          MEDIUM_HIGH = PRIORITY_CHOICES.get('MEDIUM_HIGH')
          HIGH = PRIORITY_CHOICES.get('HIGH')
     priority = models.IntegerField(choices = ToDoPriority.choices, default = ToDoPriority.LOW)
+           
