150a59db2ab130e1c5e90972eaa81a3d8473454e
Lakii997 <laki.kalajdzic@gmail.com>
2021-09-08 09:04:38 +0200

    flake8

--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,5 @@
-from django.core.checks.messages import Error
 from django.db import models
 from django.contrib.auth.models import  AbstractUser
-from django.db.models.expressions import Value
 import uuid
 from .managers import UserQuerySet
 
